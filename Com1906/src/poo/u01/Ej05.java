d236b05f2403f54604d72bb2e0f1d7ef0f4d3270
lautarovallelopez <54020991+lautarovallelopez@users.noreply.github.com>
2019-09-02 10:22:58 -0300

    Update Ej05.java

--- Com1906/src/poo/u01/Ej05.java
+++ Com1906/src/poo/u01/Ej05.java
@@ -7,6 +7,7 @@ public class Ej05 {
 		Persona p2 = new Persona("4848949","Juan", 20, "M");
 		System.out.println(p2.getEdad());
 		System.out.println(p1);
+                //esto es un comentario del primer commit
 	}
 
 }
